2e779c27253c97d3c0a3cc3c87637649184e1105
Josef Hovad <josef.hovad@nobord.com>
2017-05-20 16:26:54 +0200

    update

--- app/models/spree/page.rb
+++ app/models/spree/page.rb
@@ -18,7 +18,7 @@ class Spree::Page < ActiveRecord::Base
 
   translates :title, :body, :slug, :layout, :foreign_link, :meta_keywords, :meta_title, :meta_description, fallbacks_for_empty_translations: true
   
-  include SpreeGlobalize
+  include SpreeGlobalize::Translatable
   
   extend FriendlyId
   friendly_id :spree_page_id, :use => :globalize
