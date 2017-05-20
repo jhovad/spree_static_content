79ce0801d92f46ece9f467d9a11d53e9eda6684d
Josef Hovad <josef.hovad@nobord.com>
2017-05-20 16:17:01 +0200

    update

--- app/models/spree/page.rb
+++ app/models/spree/page.rb
@@ -17,6 +17,7 @@ class Spree::Page < ActiveRecord::Base
   scope :by_store, ->(store) { joins(:stores).where('spree_pages_stores.store_id = ?', store) }
 
   translates :title, :body, :slug, :layout, :foreign_link, :meta_keywords, :meta_title, :meta_description, fallbacks_for_empty_translations: true
+  
   include SpreeGlobalize::Translatable
   
   extend FriendlyId
